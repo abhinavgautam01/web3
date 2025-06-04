503bfdaac734b64222e60bd37879d69de6506030
abhinavgautam01 <abgautam1017@gmail.com>
2025-06-04 22:16:19 +0530

    solana-basic-prac

---
+++ practice-solana/airdrop/index.ts
@@ -0,0 +1,25 @@
+import { PublicKey, Connection, LAMPORTS_PER_SOL } from "@solana/web3.js";
+
+export const airdrop = async (address: PublicKey, amount: number) => {
+  const publicKey = new PublicKey(address);
+  const connection = new Connection("http://localhost:8899", "confirmed");
+
+  const signature = await connection.requestAirdrop(
+    publicKey,
+    amount * LAMPORTS_PER_SOL
+  );
+  
+  // await connection.confirmTransaction(signature)
+
+  const latestBlockhash = await connection.getLatestBlockhash();
+
+  await connection.confirmTransaction(
+    {
+      signature,
+      ...latestBlockhash,
+    },
+    "confirmed"
+  );
+};
+
+airdrop(new PublicKey("7kLbkkbC43RacU1pmdt6YXVhRjErdtnZgmrYKFHXxEsS"), 10);
