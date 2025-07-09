a1d29f56e2825941b75f54accb63d4385314ec77
abhinavgautam01 <abgautam1017@gmail.com>
2025-07-09 21:55:16 +0530

    wallet-adaptar

--- wallet-adapter/src/App.jsx
+++ wallet-adapter/src/App.jsx
@@ -1,31 +1,39 @@
-import React, { FC, useMemo } from 'react';
+import React, { useMemo } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
 import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
-import { UnsafeBurnerWalletAdapter } from '@solana/wallet-adapter-wallets';
 import {
     WalletModalProvider,
     WalletDisconnectButton,
     WalletMultiButton
 } from '@solana/wallet-adapter-react-ui';
 import { clusterApiUrl } from '@solana/web3.js';
-import { Airdrop } from './Airdrop';
 
-// Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
+import { SendTokens } from './SendTokens';
+import { SignMessage } from './SignMessage';
 
 function App() {
+  const network = WalletAdapterNetwork.Devnet;
+
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
 
   return (
-    <ConnectionProvider endpoint={"https://solana-devnet.g.alchemy.com/v2/Jdx7kejFF8h5mR4WqH0Ji"}>
-            <WalletProvider wallets={[]} autoConnect>
-                <WalletModalProvider>
+      <ConnectionProvider endpoint={endpoint}>
+          <WalletProvider wallets={[]} autoConnect>
+              <WalletModalProvider>
+                <div style={{ display: 'flex', justifyContent: "space-between" }}>
                   <WalletMultiButton />
-                    <WalletDisconnectButton />
-                    <Airdrop/>
-                </WalletModalProvider>
-            </WalletProvider>
-        </ConnectionProvider>
-  )
+                  <WalletDisconnectButton />
+                </div>
+                {/* <RequestAirdrop />
+                <ShowSolBalance /> */}
+                {/* <Tokens /> */}
+                <SignMessage />
+                <SendTokens />
+              </WalletModalProvider>
+          </WalletProvider>
+      </ConnectionProvider>
+  );
 }
 
 export default App
