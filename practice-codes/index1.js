a15bde5df6dcc50d0ba7490a2f0de9a0808c06c1
abhinavgautam01 <abgautam1017@gmail.com>
2025-07-11 22:33:52 +0530

    practicing-codes

--- practice-codes/index1.js
+++ practice-codes/index1.js
@@ -9,3 +9,24 @@ function asciiToBytes(asciiString){
 const ascii = "Golu"
 const byteArray = asciiToBytes(ascii)
 console.log(byteArray)
+
+function hexToBytes(hexString) {
+    if (hexString.length % 2 !== 0) {
+        throw new Error("Invalid hex string");
+    }
+
+    const bytes = [];
+    for (let i = 0; i < hexString.length; i += 2) {
+        bytes.push(parseInt(hexString.substr(i, 2), 16));
+    }
+    return new Uint8Array(bytes);
+}
+
+
+function asciiToHex(asciiString) {
+    let hex = "";
+    for (let i = 0; i < asciiString.length; i++) {
+        hex += asciiString.charCodeAt(i).toString(16).padStart(2, '0');
+    }
+    return hex;
+}
