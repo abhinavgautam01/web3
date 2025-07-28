5cc3dfa43f52c1cacd1494b9f915c319e8109c21
abhinavgautam01 <abgautam1017@gmail.com>
2025-07-28 14:23:00 +0530

    rust-macros

---
+++ practice-rust/rust-prac5-macro/app/src/main.rs
@@ -0,0 +1,20 @@
+use serialize_macro::{SerializeNumberStruct, DeserializeNumberStruct};
+use serialize_macro_traits::{Serialize, Deserialize};
+
+#[derive(SerializeNumberStruct, DeserializeNumberStruct)]
+struct Swap {
+    qty_1: i32,
+    qty_2: i32,
+    qty_3: i32
+}
+
+fn main() {
+    println!("Hello, world!");
+    let s = Swap {
+        qty_1: 1,
+        qty_2: 2,
+        qty_3: 1000
+    };
+    let bytes = s.serialize();
+    println!("{:?}", bytes);
+}
