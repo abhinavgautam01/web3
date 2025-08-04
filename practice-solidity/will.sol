5bd9c992038b124ab883c03ba04aae3a343a749a
abhinavgautam01 <abgautam1017@gmail.com>
2025-08-04 16:02:22 +0530

    will-contract-completed

--- practice-solidity/will.sol
+++ practice-solidity/will.sol
@@ -4,42 +4,44 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Will {
     address private owner;
-    address private recepient;
-    uint private amount;
-    uint private ping;
+    address payable private recepient;
+    uint tenYears;
+    uint startTime;
+    uint public lastVisited;
 
-    constructor(address _recepient) payable {
+    constructor(address payable _recepient) payable {
         owner = msg.sender;
         recepient = _recepient;
-        amount = msg.value;
         tenYears = 1 hours * 24 * 365 * 10;
         startTime = block.timestamp;
+        lastVisited = block.timestamp;
     }
 
     modifier onlyOwner() {
-        require(msg.sender == owner, "Its should be a owner");
+        require(msg.sender == owner, "Should be owner only");
         _;
     }
 
     modifier onlyRecepient() {
-        require(msg.sender == recepient, "Its should be a valid recepient");
+        require(msg.sender == recepient, "Should be a valid recepient");
         _;
     }
 
-    function changeRecepient(address _recepient)onlyOwner public {
+    function deposit () public payable onlyOwner {
+        lastVisited = block.timestamp;
+    }
+    
+    function ping()public{
+        lastVisited = block.timestamp;
+    }
+
+    function changeRecepient(address payable _recepient)onlyOwner public {
         recepient = _recepient;
     }
 
-    function pingM() onlyOwner public {
-        ping = 0;
-    }
-
-    function claim(address from)external {
-        User memory targetUser = users[from];
-        uint date = block.timestamp - tenYears;
-        require(targetUser.nominee == msg.sender);
-        require(targetUser.lastSeen < date);
-        payable(msg.sender).transfer(targetUser.amount);
+    function claim() external onlyRecepient {
+        require(lastVisited < block.timestamp - tenYears, "Can't claim, owner is still alive..!");
+        payable(recepient).transfer(address(this).balance);
     }
 
 }
