2f6b7b9153b349f3ad99067bf5a0ae3ba3ff1a46
abhinavgautam01 <abgautam1017@gmail.com>
2025-08-09 14:59:06 +0530

    completed-oneWay-bridge-contract

--- bridges/src/BridgeBase.sol
+++ bridges/src/BridgeBase.sol
@@ -2,22 +2,37 @@
 pragma solidity ^0.8.13;
 
 import { Ownable } from "node_modules/@openzeppelin/contracts/access/Ownable.sol";
+import { IERC20 } from "node_modules/@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+interface IBUSDT is IERC20 {
+    function mint(address _to, uint _amount) external;
+    function burn(address _from, uint _amount) external;
+}
 
 contract BridgeBase {
-    constructor() Ownable(msg.sender){
+    address public tokenAddress;
+    mapping (address => uint) public pendingBalances;
 
+    event Burn(address indexed from , uint _amount);
+
+    constructor(address _tokenAddress) Ownable(msg.sender){
+        tokenAddress = _tokenAddress;
     }
 
-    function mint() public {
-
+    function mint(IBUSDT _tokenAddress, uint _amount) public {
+        require(pendingBalances[msg.sender]>=_amount);
+        pendingBalances[msg.sender] -= _amount;
+        _tokenAddress.mint(msg.sender, _amount);
     }
 
-    function burn() public {
-
+    function burn(IBUSDT _tokenAddress, uint _amount) public {
+        require(_tokenAddress==tokenAddress);
+        _tokenAddress.burn(msg.sender, _amount);
+        emit Burn(msg.sender, _amount);
     }
 
-    function depositHappenedOnOtherSide() public {
-
+    function depositHappenedOnOtherSide(address userAccount, uint _amount) public {
+        pendingBalances[userAccount] += _amount;
     }
 
 }
