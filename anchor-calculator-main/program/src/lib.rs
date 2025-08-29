a766b2144a361c9bedd3c572ba8f900ff9394a8a
abhinavgautam01 <abgautam1017@gmail.com>
2025-08-29 22:27:20 +0530

    native_contract-calculator

---
+++ anchor-calculator-main/program/src/lib.rs
@@ -0,0 +1,63 @@
+use borsh::{BorshDeserialize, BorshSerialize};
+use solana_program::{
+    entrypoint, 
+    account_info::{
+        next_account_info,
+        AccountInfo
+    }, 
+    entrypoint::ProgramResult, 
+    pubkey::Pubkey,
+    program_error::ProgramError,
+};
+
+#[derive(BorshSerialize, BorshDeserialize)]
+struct CounterState {
+    count: u32
+}
+
+#[derive(BorshSerialize, BorshDeserialize)]
+enum Instruction {
+    Init,
+    Double,
+    Half,
+    Add { amount: u32 },
+    Subtract { amount: u32 },
+}
+
+entrypoint!(process_instruction);
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+    let iter = &mut accounts.iter();
+    let data_account = next_account_info(iter)?;
+    
+    if !data_account.is_signer {
+        return Err(ProgramError::MissingRequiredSignature);
+    }
+    
+    let mut counter_state = CounterState::try_from_slice(&data_account.data.borrow())?;
+    
+    let instruction = Instruction::try_from_slice(&instruction_data)?;
+    match instruction {
+        Instruction::Init => {
+            counter_state.count = 1;
+        },
+        Instruction::Double => {
+            counter_state.count = counter_state.count.saturating_mul(2);
+        },
+        Instruction::Half => {
+            counter_state.count = counter_state.count / 2;
+        },
+        Instruction::Add { amount } => {
+            counter_state.count = counter_state.count.saturating_add(amount);
+        },
+        Instruction::Subtract { amount } => {
+            counter_state.count = counter_state.count.saturating_sub(amount);
+        },
+    }
+    
+    counter_state.serialize(&mut *data_account.data.borrow_mut())?;
+    Ok(())
+}
